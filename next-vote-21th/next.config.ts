8ecf8a2188183e82b3bc523c1c7a20b929edb934
chaeyoungwon <epopcy@naver.com>
2025-05-21 16:58:37 +0900

    feat: 헤더 구조 추가

--- next-vote-21th/next.config.ts
+++ next-vote-21th/next.config.ts
@@ -1,21 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config: any) => {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg"),
+    );
+
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
+      issuer: fileLoaderRule.issuer,
+      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
       use: ["@svgr/webpack"],
     });
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
     return config;
-  },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.ts",
-      },
-    },
   },
 };
 
---
+++ next-vote-21th/src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/public/svgs/header/logo.svg";
+
+const Header = () => {
+  return (
+    <nav className="px-[21px] py-[22px]">
+      <Logo />
+    </nav>
+  );
+};
+
+export default Header;
