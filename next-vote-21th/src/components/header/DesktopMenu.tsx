89c10facde36be90581a92f62932b3befe4a1879
chaeyoungwon <epopcy@naver.com>
2025-05-21 23:12:24 +0900

    feat: 페이지에 들어와있을 경우 헤더 메뉴에 밑줄 추가

--- next-vote-21th/src/components/header/DesktopMenu.tsx
+++ next-vote-21th/src/components/header/DesktopMenu.tsx
@@ -1,18 +1,28 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import { menuItems } from "src/constants/header/menuItems";
+import { menuItems } from "@/constants/header/menuItems";
 
-const DesktopMenu = () => (
-  <ul className="hidden gap-9 pr-[29px] min-md:flex">
-    {menuItems.map(({ label, href }) => (
-      <li
-        key={label}
-        className="text-body1-med text-violet-dark w-[100px] text-center hover:underline min-md:!text-[20px]"
-      >
-        <Link href={href}>{label}</Link>
-      </li>
-    ))}
-  </ul>
-);
+const DesktopMenu = () => {
+  const pathname = usePathname();
+  return (
+    <ul className="hidden gap-9 pr-[29px] min-md:flex">
+      {menuItems.map(({ label, href }) => {
+        const isActive = pathname === href;
+
+        return (
+          <li
+            key={label}
+            className={`text-body1-med text-violet-dark w-[100px] text-center hover:underline min-md:!text-[20px] ${
+              isActive ? "font-semibold underline" : ""
+            }`}
+          >
+            <Link href={href}>{label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default DesktopMenu;
