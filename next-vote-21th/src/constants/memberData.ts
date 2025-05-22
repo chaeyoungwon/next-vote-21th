874a520dd1dc2944ddf33e5d2acad7a28fd82406
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-05-23 06:10:59 +0900

    feat: 멤버 데이터 추가

---
+++ next-vote-21th/src/constants/memberData.ts
@@ -0,0 +1,26 @@
+export const FE = {
+  원채영:{college: "Hongik Univ."},
+  신수진:{college: "Hongik Univ."},
+  최서연:{college: "Hongik Univ."},
+  한서정:{college: "Ewha. W. Univ."},
+  김철흥:{college: "Hongik Univ."},
+  송아영:{college: "Hongik Univ."},
+  김영서:{college: "Ewha. W. Univ."},
+  이주희:{college: "Hongik Univ."},
+  권동욱:{college: "Yonsei Univ."},
+  김서연:{college: "Ewha. W. Univ."}
+
+}
+
+export const BE = {
+  최근호:{college: "Hongik Univ."},
+  이석원:{college: "Yonsei Univ."},
+  박서연:{college: "Hongik Univ."},
+  박채연:{college: "Hongik Univ."},
+  김준형:{college: "Hongik Univ."},
+  임도현:{college: "Hongik Univ."},
+  박정하:{college: "Hongik Univ."},
+  서채연:{college: "Hongik Univ."},
+  오지현:{college: "Hongik Univ."},
+  한혜수:{college: "Hongik Univ."}
+}
