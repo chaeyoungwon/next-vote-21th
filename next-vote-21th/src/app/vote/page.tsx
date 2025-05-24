cf6524a6622b904745ecaa8c2eaa13f6d9a695a8
chaeyoungwon <epopcy@naver.com>
2025-05-25 00:04:04 +0900

    fix: vote 첫 페이지 리팩토링 및 디자인 수정

--- next-vote-21th/src/app/vote/page.tsx
+++ next-vote-21th/src/app/vote/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 
 import BackgroundShapes from "@/components/vote/BackgroundShape";
+import VoteButton from "@/components/vote/VoteButton";
 
 type Part = "frontend" | "backend" | "demoday";
 
@@ -18,46 +19,44 @@ const VotePage = () => {
   };
 
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center max-md:mt-31 md:justify-center">
+    <div className="relative flex min-h-screen w-full flex-col items-center overflow-hidden max-md:mt-31 md:justify-center">
       <BackgroundShapes />
       <div className="flex w-[218px] flex-col max-md:ml-[-108px]">
         <div className="text-heading3 md:text-heading1 text-left">VOTE</div>
+
+        {/* 파트장 투표 */}
         <div className="flex flex-col gap-5 pt-[27px]">
           <div className="text-violet-pressed text-heading3 md:text-heading1">
             파트장 투표
           </div>
-          <button
+          <VoteButton
             onClick={() => handleClick("frontend")}
-            className="text-heading3 border-violet-dark text-violet-dark bg-violet-light relative flex h-11 w-[218px] cursor-pointer items-center justify-center gap-[14px] rounded-[24px] border border-solid"
-          >
-            <div>프론트엔드 파트장</div>
-            <div className="absolute top-1/2 right-[19px] -translate-y-1/2">
-              &gt;
-            </div>
-          </button>
-          <button
+            text="프론트엔드 파트장"
+            borderColor="border-violet-dark"
+            textColor="text-violet-dark"
+            bgColor="bg-violet-light"
+          />
+          <VoteButton
             onClick={() => handleClick("backend")}
-            className="text-heading3 border-violet-dark text-violet-dark bg-violet-light relative flex h-11 w-[218px] cursor-pointer items-center justify-center gap-[14px] rounded-[24px] border border-solid"
-          >
-            <div>백엔드 파트장</div>
-            <div className="absolute top-1/2 right-[19px] -translate-y-1/2">
-              &gt;
-            </div>
-          </button>
+            text="백엔드 파트장"
+            borderColor="border-violet-dark"
+            textColor="text-violet-dark"
+            bgColor="bg-violet-light"
+          />
         </div>
+
+        {/* 데모데이 투표 */}
         <div className="flex flex-col gap-5 pt-[35px]">
           <div className="text-green-dark text-heading3 md:text-heading1">
             데모데이 투표
           </div>
-          <button
+          <VoteButton
             onClick={() => handleClick("demoday")}
-            className="text-heading3 border-green-dark text-green-dark bg-green-light relative flex h-11 w-[218px] cursor-pointer items-center justify-center gap-[14px] rounded-[24px] border border-solid"
-          >
-            <div>데모데이</div>
-            <div className="absolute top-1/2 right-[19px] -translate-y-1/2">
-              &gt;
-            </div>
-          </button>
+            text="데모데이"
+            borderColor="border-green-dark"
+            textColor="text-green-dark"
+            bgColor="bg-green-light"
+          />
         </div>
       </div>
     </div>
