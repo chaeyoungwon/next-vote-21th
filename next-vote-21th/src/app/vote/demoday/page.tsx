9854b4bca88255ef48ae1c87f6ce7dbd5de6d73e
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-05-24 23:15:02 +0900

    refactor: 데모데이 페이지 title추가

--- next-vote-21th/src/app/vote/demoday/page.tsx
+++ next-vote-21th/src/app/vote/demoday/page.tsx
@@ -1,7 +1,7 @@
 const DemodayVotePage = () => {
   return (
     <div className="mt-16 flex min-h-screen w-screen flex-col">
-      데모데이투표페이지입니다.
+      <div>21th CEOS PROJECT</div>
     </div>
   );
 };
