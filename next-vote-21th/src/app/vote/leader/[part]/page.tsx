326ff294dbcf6f608ec9ccf9baa2179d88d318a3
chaeyoungwon <epopcy@naver.com>
2025-05-25 01:41:57 +0900

    fix: 투표 순위 보러 가기 문구 위치 수정

--- next-vote-21th/src/app/vote/leader/[part]/page.tsx
+++ next-vote-21th/src/app/vote/leader/[part]/page.tsx
@@ -45,50 +45,51 @@ const LeaderVotePage = () => {
     <div className="relative flex min-h-screen w-screen flex-col items-center justify-center overflow-hidden">
       <BackgroundShapes />
 
-      {/* 헤더 + 투표하기 버튼 */}
-      <div className="flex flex-row justify-center gap-[47px] pb-7">
-        <div className="text-heading3 md:text-heading1 text-violet-pressed">
-          {title}
-        </div>
-        <button
-          onClick={() => !hasVoted && setIsModalOpen(true)}
-          disabled={!selectedCandidate || hasVoted}
-          className={`text-lab1-sb underline transition-opacity duration-200 ${
-            hasVoted
-              ? "pointer-events-none opacity-0"
-              : selectedCandidate
-                ? "cursor-pointer text-black"
-                : "cursor-not-allowed text-gray-700"
-          }`}
-        >
-          투표하기
-        </button>
-      </div>
-      {/* 후보 목록 */}
-      <div className="grid grid-cols-2 gap-x-[21px] gap-y-[7px]">
-        {candidates.map(name => (
+      <div className="flex flex-col items-center justify-center">
+        {/* 헤더 + 투표하기 버튼 */}
+        <div className="flex flex-row justify-center gap-[47px] pb-7">
+          <div className="text-heading3 md:text-heading1 text-violet-pressed">
+            {title}
+          </div>
           <button
-            key={name}
-            onClick={() => setSelectedCandidate(name)}
-            className={`text-heading3 border-violet-pressed h-[41px] w-[114px] cursor-pointer rounded-[24px] border md:h-[50px] md:w-[120px] ${
-              selectedCandidate === name
-                ? "bg-violet-pressed text-white"
-                : "bg-violet-light text-violet-pressed hover:bg-violet-pressed hover:text-white"
+            onClick={() => !hasVoted && setIsModalOpen(true)}
+            disabled={!selectedCandidate || hasVoted}
+            className={`text-lab1-sb underline transition-opacity duration-200 ${
+              hasVoted
+                ? "pointer-events-none opacity-0"
+                : selectedCandidate
+                  ? "cursor-pointer text-black"
+                  : "cursor-not-allowed text-gray-700"
             }`}
           >
-            {name}
+            투표하기
           </button>
-        ))}
+        </div>
+        {/* 후보 목록 */}
+        <div className="grid grid-cols-2 gap-x-[21px] gap-y-[7px]">
+          {candidates.map(name => (
+            <button
+              key={name}
+              onClick={() => setSelectedCandidate(name)}
+              className={`text-heading3 border-violet-pressed h-[41px] w-[114px] cursor-pointer rounded-[24px] border md:h-[50px] md:w-[120px] ${
+                selectedCandidate === name
+                  ? "bg-violet-pressed text-white"
+                  : "bg-violet-light text-violet-pressed hover:bg-violet-pressed hover:text-white"
+              }`}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+
+        {/* 결과 페이지 이동 */}
+        <Link
+          className="text-heading3 md:text-heading2 text-violet-pressed flex self-end pt-[45px] text-right max-md:ml-[561x]"
+          href={`/vote/leader/${part}/result`}
+        >
+          현재 투표 순위 보러 가기 &gt;
+        </Link>
       </div>
-
-      {/* 결과 페이지 이동 */}
-      <Link
-        className="text-heading3 md:text-heading2 text-violet-pressed ml-[106px] pt-[45px] text-right max-md:ml-[561x]"
-        href={`/vote/leader/${part}/result`}
-      >
-        현재 투표 순위 보러 가기 &gt;
-      </Link>
-
       {/* 투표 확인 모달 */}
       {isModalOpen && selectedCandidate && (
         <VoteModal
