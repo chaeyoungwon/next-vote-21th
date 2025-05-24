a007edc6558107bbb64bb471504ebf358a52508c
chaeyoungwon <epopcy@naver.com>
2025-05-25 00:26:06 +0900

    feat: 데모데이 투표하기 구현

--- next-vote-21th/src/app/vote/demoday/page.tsx
+++ next-vote-21th/src/app/vote/demoday/page.tsx
@@ -1,8 +1,79 @@
+"use client";
+
+import { useState } from "react";
+
+import clsx from "clsx";
+
+import VoteModal from "@/components/common/VoteModal";
+
+import { teamList } from "@/constants/signup/teamLists";
+
 const DemodayVotePage = () => {
+  const teamNames = Object.keys(teamList["Front-End"]);
+  const [selectedTeam, setSelectedTeam] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
+
+  const handleVote = (team: string | null) => {
+    if (!team) return;
+    // TODO: 여기에 투표 처리 로직 (API 호출 등) 작성
+    console.log(`${team} 팀에 투표 완료`);
+    setHasVoted(true);
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="flex min-h-screen w-screen flex-col">
-      <div>21th CEOS PROJECT</div>
+    <div className="flex min-h-screen w-screen flex-col items-center pt-16">
+      <div className="mt-[152px] flex flex-col">
+        {/* 헤더 */}
+        <div className="flex items-center justify-center">
+          <div className="mb-7 ml-[-26px] flex items-center justify-center gap-9 text-center md:ml-[-52px]">
+            <div className="text-heading3 md:text-heading1 text-green-dark">
+              21th CEOS PROJECT
+            </div>
+            <span
+              onClick={() => !hasVoted && setIsModalOpen(true)}
+              className={clsx(
+                "text-lab1-b md:text-body1-sb cursor-pointer underline transition-opacity duration-200",
+                hasVoted && "pointer-events-none opacity-0",
+              )}
+            >
+              투표하기
+            </span>
+          </div>
+        </div>
+
+        {/* 팀 리스트 */}
+        <div className="flex flex-col gap-4">
+          {teamNames.map(team => (
+            <div
+              key={team}
+              onClick={() => {
+                setSelectedTeam(team);
+              }}
+              className={clsx(
+                "text-heading3 flex h-[44px] w-[261px] cursor-pointer items-center justify-center rounded-3xl border md:h-[50px]",
+                "border-green-dark",
+                selectedTeam === team
+                  ? "bg-green text-white"
+                  : "bg-green-light text-green-dark hover:bg-green hover:text-white",
+              )}
+            >
+              {team}
+            </div>
+          ))}
+        </div>
+        {isModalOpen && selectedTeam && (
+          <VoteModal
+            target={selectedTeam}
+            targetType="팀"
+            onClose={() => setIsModalOpen(false)}
+            onConfirm={() => handleVote(selectedTeam)}
+          />
+        )}
+      </div>
     </div>
   );
 };
+
 export default DemodayVotePage;
--- next-vote-21th/src/components/common/VoteModal.tsx
+++ next-vote-21th/src/components/common/VoteModal.tsx
@@ -1,4 +1,6 @@
 interface VoteModalProps {
+  target: string;
+  targetType: "팀" | "파트장";
   onClose: () => void;
   onConfirm: () => void;
 }
@@ -10,22 +12,24 @@ const VoteModal = ({ onClose, onConfirm }: VoteModalProps) => {
       style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
     >
       <div className="h-[280px] w-[320px] overflow-hidden rounded-3xl bg-white md:h-[320px] md:w-[400px]">
-        <div className="text-heading1 flex h-[217px] w-full flex-col items-center justify-center md:h-[257px]">
+        {/* 모달 본문 */}
+        <div className="text-heading2 flex h-[217px] w-full flex-col items-center justify-center md:h-[257px]">
           <div>투표는 분야별 1회만 가능하며,</div>
           <div>제출 후에는 수정이 어렵습니다.</div>
           <br />
           <div>투표하시겠습니까?</div>
         </div>
 
-        <div className="text-heading2 h-[63px] w-full items-center justify-end">
+        {/* 버튼 영역 */}
+        <div className="text-heading2 flex h-[63px] w-full">
           <button
-            className="h-full w-1/2 cursor-pointer bg-gray-300 text-gray-0 hover:bg-gray-700"
+            className="text-gray-0 h-full w-1/2 cursor-pointer bg-gray-300 hover:bg-gray-700"
             onClick={onClose}
           >
             아니오
           </button>
           <button
-            className="bg-green hover:bg-green-dark h-full text-gray-0  w-1/2 cursor-pointer"
+            className="bg-green hover:bg-green-dark text-gray-0 h-full w-1/2 cursor-pointer"
             onClick={onConfirm}
           >
             예
