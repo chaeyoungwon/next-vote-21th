5dcfdd351300fa6eb21ea2f8eb671a38d7bfda50
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-06-23 19:03:31 +0900

    refactor: 모달 공통모달형식으로 코드 수정

---
+++ next-vote-21th/src/components/common/CommonModal.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import { modalData } from "@/constants/modalData";
+
+interface ButtonConfig {
+  label: string;
+  onClick: () => void;
+  className: string;
+}
+
+interface CommonModalProps {
+  messageKey: keyof typeof modalData;
+  buttons: ButtonConfig[];
+}
+
+const CommonModal: React.FC<CommonModalProps> = ({ messageKey, buttons }) => {
+  return (
+    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/60">
+      <div className="flex h-[280px] w-[320px] flex-col overflow-hidden rounded-3xl bg-white md:h-[320px] md:w-[400px]">
+        {/* 모달 본문 */}
+        <div className="text-heading2 flex h-[217px] w-full flex-col items-center justify-center text-center md:h-[257px]">
+          <div className="flex flex-col items-center justify-center gap-2">
+            {modalData[messageKey].split("<br />").map((line, index) => (
+              <span key={index}>{line}</span>
+            ))}
+          </div>
+        </div>
+
+        {/* 버튼 영역 */}
+        {buttons.length === 1 ? (
+          <button
+            className={`text-heading2 text-gray-0 h-[63px] w-full cursor-pointer ${buttons[0].className}`}
+            onClick={buttons[0].onClick}
+          >
+            {buttons[0].label}
+          </button>
+        ) : (
+          <div className="text-heading2 text-gray-0 flex h-[63px] w-full items-center justify-end">
+            {buttons.map((button, index) => (
+              <button
+                key={index}
+                className={`h-full w-1/2 cursor-pointer ${button.className}`}
+                onClick={button.onClick}
+              >
+                {button.label}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CommonModal;
--- next-vote-21th/src/components/common/LoginModal.tsx
+++ next-vote-21th/src/components/common/LoginModal.tsx
@@ -1,3 +1,5 @@
+import CommonModal from "./CommonModal";
+
 interface LoginModalProps {
   onClose: () => void;
   onConfirm: () => void;
@@ -5,30 +7,21 @@ interface LoginModalProps {
 
 const LoginModal = ({ onClose, onConfirm }: LoginModalProps) => {
   return (
-    <div className="fixed inset-0 z-100 flex items-center justify-center bg-black/60">
-      <div className="h-[280px] w-[320px] overflow-hidden rounded-3xl bg-white md:h-[320px] md:w-[400px]">
-        <div className="text-heading2 flex h-[217px] w-full flex-col items-center justify-center md:h-[257px]">
-          <div>로그인이 필요한 서비스입니다.</div>
-          <br />
-          <div>로그인 하시겠습니까?</div>
-        </div>
-
-        <div className="text-heading2 text-gray-0 h-[63px] w-full items-center justify-end">
-          <button
-            className="text-gray-0 h-full w-1/2 cursor-pointer bg-gray-300 hover:bg-gray-700"
-            onClick={onClose}
-          >
-            아니오
-          </button>
-          <button
-            className="bg-green hover:bg-green-dark text-gray-0 h-full w-1/2 cursor-pointer"
-            onClick={onConfirm}
-          >
-            예
-          </button>
-        </div>
-      </div>
-    </div>
+    <CommonModal
+      messageKey="login"
+      buttons={[
+        {
+          label: "아니오",
+          onClick: onClose,
+          className: "bg-gray-300 text-gray-0 hover:bg-gray-700",
+        },
+        {
+          label: "예",
+          onClick: onConfirm,
+          className: "bg-green text-gray-0 hover:bg-green-dark",
+        },
+      ]}
+    />
   );
 };
 
